a810eb996a15d059f89fdba8a7c75816e1b01115
Guilherme Condé <guicondee@outlook.com>
2023-01-23 21:32:51 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { Button } from "./Components/Button"
+
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/global"
+
+
+export function App() {
+  return (
+
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider >
+  )
+}
+
+
