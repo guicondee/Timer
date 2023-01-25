e068429dd3132163189b10ab4ab852d260806c41
Guilherme Condé <guicondee@outlook.com>
2023-01-24 22:42:09 -0300

    import all componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { Button } from './Components/Button'
 
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
+
 }
