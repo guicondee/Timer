cd4f0aed31ec63c4c362b556854463dce1cef02a
Guilherme Condé <guicondee@outlook.com>
2023-02-08 22:02:41 -0300

    create context home

--- src/pages/Home/components/NewCycleForm/styles.ts
+++ src/pages/Home/components/NewCycleForm/styles.ts
@@ -11,3 +11,36 @@ export const FormContainer = styled.div`
   font-weight: bold;
   flex-wrap: wrap;
 `
+
+export const BaseInput = styled.input`
+  background: transparent;
+  height: 2.5rem;
+  border: none;
+  border-bottom: 2px solid ${(props) => props.theme["gray-500"]};
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme["gray-100"]};
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme["green-500"]};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme["gray-500"]};
+  }
+`
+
+export const TaskInput = styled(BaseInput)`
+ flex: 1;
+
+ &::-webkit-calendar-picker-indicator {
+  display: none !important;
+ }
+`
+
+export const MinutesAmountInput = styled(BaseInput)`
+   width: 4rem;
+
+`
