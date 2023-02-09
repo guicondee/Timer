0d90ab5db6e7c1f7f70cf9b34a8bd5e4ac27be84
Guilherme Condé <guicondee@outlook.com>
2023-02-08 21:59:53 -0300

    ajusting styles components

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -56,36 +56,7 @@ export const StopCountdowButton = styled(BaseCountdowButton)`
   }
 `
 
-export const BaseInput = styled.input`
-  background: transparent;
-  height: 2.5rem;
-  border: none;
-  border-bottom: 2px solid ${(props) => props.theme["gray-500"]};
-  font-weight: bold;
-  font-size: 1.125rem;
-  padding: 0 0.5rem;
-  color: ${(props) => props.theme["gray-100"]};
 
-  &:focus {
-    box-shadow: none;
-    border-color: ${(props) => props.theme["green-500"]};
-  }
 
-  &::placeholder {
-    color: ${(props) => props.theme["gray-500"]};
-  }
-`
 
-export const TaskInput = styled(BaseInput)`
- flex: 1;
-
- &::-webkit-calendar-picker-indicator {
-  display: none !important;
- }
-`
-
-export const MinutesAmountInput = styled(BaseInput)`
-   width: 4rem;
-
-`
 
