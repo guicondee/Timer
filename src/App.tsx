2ebac07ac105293101fb83e3dffc03798c126fd0
Guilherme Condé <guicondee@outlook.com>
2023-02-18 14:30:12 -0300

    modification structure add providers

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,17 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Router } from './Router'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 export function App() {
+
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
