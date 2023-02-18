469afaaaf342588f846712641f32345382e2d835
Guilherme Condé <guicondee@outlook.com>
2023-02-18 14:31:49 -0300

    structure context Cycle

--- src/pages/Home/components/Countdow/index.tsx
+++ src/pages/Home/components/Countdow/index.tsx
@@ -1,6 +1,6 @@
 import { differenceInSeconds } from "date-fns";
 import { useContext, useEffect } from "react";
-import { CyclesContext } from "../..";
+import { CyclesContext } from "../../../../contexts/CyclesContext";
 import { CountDowContainer, Separator } from "./styles";
 
 export function Countdow() {
